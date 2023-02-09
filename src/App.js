2fcda6e0b3b000017e865801687945bb189817a6
kodegosp404 <kodegosp404@gmail.com>
2023-02-09 11:01:37 +0800

    Edit and Delete

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ import "./App.css";
 
 function App() {
   const [tasklist, setTasklist] = useState([]);
+  const [task, setTask] = useState({});
 
   return (
     <div className="App">
       <Header />
-      <AddTask tasklist={tasklist} setTasklist={setTasklist} />
-      <ShowTask tasklist={tasklist} setTasklist={setTasklist} />
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
